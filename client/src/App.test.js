72eaf27f5a33275f99b14c159c432452aacae885
Christian Weigandt <christianrw84@gmail.com>
2021-09-28 00:20:17 -0400

    CI for unit tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { shallow } from 'enzyme'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('A[[', () => {
+  it('renders App', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('.App')).toHaveLength(1)
+  })
 })

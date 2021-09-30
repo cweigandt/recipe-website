23391b54dead0c2ba847efed1bf636614267d62c
Christian Weigandt <christianrw84@gmail.com>
2021-09-30 19:36:20 -0400

    More App unit tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,39 @@
 import { shallow } from 'enzyme'
+import { Route } from 'react-router-dom'
 import App from './App'
+import NavBar from './components/navbar/NavBar'
 
-describe('A[[', () => {
+const routePaths = [
+  '/recipe/:recipeName',
+  '/grid',
+  '/tags',
+  '/upload',
+  '/edit',
+  '/sections/:sectionName',
+  '/tag/:tagName',
+  '/',
+]
+describe('App', () => {
   it('renders App', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find('.App')).toHaveLength(1)
+  })
+
+  it('renders NavBar', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(NavBar)).toHaveLength(1)
+  })
+
+  it('renders routes', () => {
+    const wrapper = shallow(<App />)
+    const routes = wrapper.find(Route)
+    expect(routes).toHaveLength(9)
+
+    routes.forEach((route) => {
+      const routeProps = route.props()
+      expect(
+        !routeProps.path || routePaths.includes(routeProps.path)
+      ).toBeTruthy()
+    })
   })
 })

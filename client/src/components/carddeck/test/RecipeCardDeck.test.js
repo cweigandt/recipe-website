f5fbc03534dec89e264c1e8cc245b28832c49701
Christian Weigandt <christianrw84@gmail.com>
2021-10-01 21:07:19 -0400

    Card deck sort unit tests

--- client/src/components/carddeck/test/RecipeCardDeck.test.js
+++ client/src/components/carddeck/test/RecipeCardDeck.test.js
@@ -3,6 +3,7 @@ import { MemoryRouter as Router } from 'react-router-dom'
 import RecipeCard from '../RecipeCard'
 import RecipeCardDeck from '../RecipeCardDeck'
 
+const rightNow = new Date().getTime()
 const mockRecipes = [
   {
     name: 'First',
@@ -16,24 +17,26 @@ const mockRecipes = [
     section: 'Drinks',
     time: '1h',
     uploader: 'me',
-    uploadTime: new Date().getTime(),
+    uploadTime: rightNow,
+  },
+  {
+    name: 'Second',
+    imageLocation: '',
+    thumbnail: '',
+    tags: [],
+    visits: 3,
+    cookedDates: [],
+    steps: [],
+    ingredients: [],
+    section: 'Entrees',
+    time: '1h 30m',
+    uploader: 'you',
+    uploadTime: rightNow - 10,
   },
 ]
-const mockWindow = () => {
-  const windowSpy = jest.spyOn(global, 'window', 'get')
-  windowSpy.mockImplementation(() => ({
-    ...window,
-    serverData: {
-      allRecipes: mockRecipes,
-    },
-  }))
-}
+
 describe('RecipeCardDeck', () => {
-  beforeEach(() => {})
-
   it('renders without error', () => {
-    // mockWindow()
-
     const rendered = shallow(<RecipeCardDeck optionalRecipes={mockRecipes} />)
     expect(rendered).not.toBeNull()
   })
@@ -45,5 +48,36 @@ describe('RecipeCardDeck', () => {
       </Router>
     )
     expect(rendered.find(RecipeCard)).toHaveLength(mockRecipes.length)
+  })
+
+  describe('Sort Bar', () => {
+    it('Sorts by upload by default', () => {
+      const rendered = mount(
+        <Router>
+          <RecipeCardDeck optionalRecipes={mockRecipes} />
+        </Router>
+      )
+
+      const cards = rendered.find(RecipeCard)
+      expect(cards).toHaveLength(mockRecipes.length)
+
+      expect(cards.at(0).props().name).toBe('First')
+      expect(cards.at(1).props().name).toBe('Second')
+    })
+
+    it('Sorts by visits on click', () => {
+      const rendered = mount(
+        <Router>
+          <RecipeCardDeck optionalRecipes={mockRecipes} />
+        </Router>
+      )
+
+      rendered.find(`[data-test-id='sort-bubble-Visits']`).simulate('click')
+      const cards = rendered.find(RecipeCard)
+      expect(cards).toHaveLength(mockRecipes.length)
+
+      expect(cards.at(0).props().name).toBe('Second')
+      expect(cards.at(1).props().name).toBe('First')
+    })
   })
 })

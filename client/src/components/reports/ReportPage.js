035f6f20b56498b8c1c6443564302a44498c5849
Christian Weigandt <christianrw84@gmail.com>
2021-12-12 02:34:36 -0500

    font sizing for word cloud. other css

--- client/src/components/reports/ReportPage.js
+++ client/src/components/reports/ReportPage.js
@@ -1,44 +1,11 @@
-import WordCloud from 'react-d3-cloud'
-import { withRouter } from 'react-router'
-import { getAllTagCounts } from '../../utilities/RecipesUtilities'
+import TagWordCloud from './TagWordCloud'
 
-import '../../styles/reports/WordCloud.css'
-
-const vw = Math.max(
-  document.documentElement.clientWidth || 0,
-  window.innerWidth || 0
-)
-const vh = Math.max(
-  document.documentElement.clientHeight || 0,
-  window.innerHeight || 0
-)
-
-const ReportPage = ({ history }) => {
-  const tags = getAllTagCounts()
-
-  const data = Object.keys(tags)
-    .map((key) => ({
-      text: key,
-      value: tags[key],
-    }))
-    .sort((a, b) => a.value - b.value)
-    .slice(-200)
-
+const ReportPage = () => {
   return (
-    <div className='word-cloud'>
-      <WordCloud
-        data={data}
-        width={vw / 2}
-        height={vh / 2}
-        padding={2}
-        fontSize={(word) => word.value * 1.5}
-        rotate={(word) => (word.value % 90) - 45}
-        onWordClick={(event, word) => {
-          history.push('/tag/' + word.text.replace(/ /g, '_'))
-        }}
-      />
+    <div className='report-page'>
+      <TagWordCloud />
     </div>
   )
 }
 
-export default withRouter(ReportPage)
+export default ReportPage

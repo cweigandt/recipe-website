7bb1180ea1b0cf01645405077a5df818ce817812
Christian Weigandt <christianrw84@gmail.com>
2022-08-09 22:37:08 -0400

    regex fix and node bump

--- client/src/utilities/RecipesUtilities.ts
+++ client/src/utilities/RecipesUtilities.ts
@@ -26,6 +26,6 @@ export const getAllTagCounts = (recipes: PartialRecipe[]) => {
 
 export const convertURLLinks = (str: string) => {
   var expression =
-    /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))/gi
+    /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*))/gi
   return str.replace(expression, `<a href="$1" target="_blank">$1</a>`)
 }
